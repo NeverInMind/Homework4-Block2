4ecf05cb03b30e3e267aebec1b24cc54510ef94b
NeverInMind <133032745+NeverInMind@users.noreply.github.com>
2023-09-19 13:33:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.parse
 import pathlib
 import mimetypes
+import socket
+
+UDP_IP = '127.0.0.1'
+UDP_PORT = 8080
+
 
 class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -15,6 +20,18 @@ class HttpHandler(BaseHTTPRequestHandler):
                 self.send_static()
             else:
                 self.send_html_file('error.html', 404)
+
+
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        print(data)
+        data_parse = urllib.parse.unquote_plus(data.decode())
+        print(data_parse)
+        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        print(data_dict)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
 
     def send_static(self):
         self.send_response(200)
@@ -34,9 +51,34 @@ class HttpHandler(BaseHTTPRequestHandler):
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
 
+def run_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    sock.bind(server)
+    try:
+        while True:
+            data, address = sock.recvfrom(1024)
+            print(f'Received data: {data.decode()} from: {address}')
+            sock.sendto(data, address)
+            print(f'Send data: {data.decode()} to: {address}')
+
+    except KeyboardInterrupt:
+        print(f'Destroy server')
+    finally: 
+        sock.close()
+
+def run_client(ip, port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    sock.sendto(message, server)
+    print(f'Send data: {message} to server: {server}')
+    response, address = sock.recvfrom(1024)
+    print(f'Response data: {response.decode()} from address: {address}')
+    sock.close()
+
 
 def run(server_class=HTTPServer, handler_class=HttpHandler):
-    server_address = ('', 3000)
+    server_address = (UDP_IP, UDP_PORT)
     http = server_class(server_address, handler_class)
     try:
         http.serve_forever()
